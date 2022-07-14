648bb3599083e4c33434fbd1c0dee288a42f78f3
Hanna Wilson <57196027+hannalwilson@users.noreply.github.com>
2022-07-14 06:53:34 -0700

    added beta 2 release to news

--- src/views/BetaView.vue
+++ src/views/BetaView.vue
@@ -1,31 +1,47 @@
 <template>
-<div>
-<p class="darkBlueHeader spreadText">DOWNLOAD BETA VERSION</p>
-<p>The MainNet Beta is finally here! <br><br>
-<strong>NOTE:</strong> Please read these instructions in full to ensure you can download and install the game properly. <br><br>
-Requirements to play: <br>
-1. Wallet Connect access via Pera Algo Wallet
-<br>2. At least 3 unique Alchemon NFT Cards (Store: <a href="https://www.randgallery.com/algo-collection/?address=ALCHY5SJXOXZXADZPD73KO6CYNZXDUWFYANTSXU6RIO3EZACIIXUCS3YDM" target="_blank">NFTs)</a>
-<br><br>Due to the fact that this is not a finished game, some operating systems attempt to block the download and installation process, thinking that the game is a virus. You can feel free to run your own virus scan. We are in the process of becoming a verified publisher.
-<br><br>This game does not operate on mobile platforms, only laptops/desktops. Here are some tips for successfully downloading and installing the video game:
-<br><br>WINDOWS 10 USERS:
-<br>1. If the file won't download, temporarily disable Windows Defender by following the instructions here: <a href="https://support.microsoft.com/en-us/windows/turn-off-defender-antivirus-protection-in-windows-security-99e6004f-c54c-8509-773c-a4d776b77960"  target="_blank">Turn off Defender Antivirus Protection in Windows 10 Security</a>
-<br>2. After you turn this off, the file should download with no issues. Once you open it, you may receive a "Windows protected your PC" warning. If that happens, click "More info" and then select "Run."
-<br>3. Turn Windows Defender back on.
-<br><br>WINDOWS 11 USERS:
-<br>1.  If the file won't download, temporarily disable Windows Defender by following the instructions here: <a href="https://allthings.how/how-to-enable-or-disable-microsoft-defender-in-windows-11/"  target="_blank">How to Enable or Disable Microsoft Defender in Windows 11</a>
-<br>2. After you turn this off, the file should download with no issues. Once you open it, you may receive a "Windows protected your PC" warning. If that happens, click "More info" and then select "Run."
-<br>3. Turn Windows Defender back on.
-<br><br>MAC USERS:
-<br>After the file downloads, right click the download and select open. The first time you try to open it, you just get a pop up warning that it can't be scanned for malicious files. However, if you click on it a second time, the pop up displays an open button and you are able to click that and launch the game.
-<br><br>DOWNLOAD LINK:
-For Windows and Mac, you may also get notifications like "This download may not be safe," which will require you to click "Keep" or "Download anyways."</p> <br><br>
-<a href="https://alch-executables.s3.amazonaws.com/AlchemonBeta_1.0.1.exe" download>
-<img src="../assets/pc.png" alt="Windows download" width="40%" height="auto"></a><br>
-<a href="https://alch-executables.s3.amazonaws.com/AlchemonBeta_1.0.1.zip" download>
-<img src="../assets/mac.png" alt="Mac download" width="40%" height="auto">
-</a>
-</div>
+  <div>
+    <p class="darkBlueHeader spreadText">DOWNLOAD BETA VERSION</p>
+    <p>The MainNet Beta is finally here! <br><br>
+      <strong>NOTE:</strong> Please read these instructions in full to ensure you can download and install the game
+      properly. <br><br>
+      Requirements to play: <br>
+      1. Wallet Connect access via Pera Algo Wallet
+      <br>2. At least 3 unique Alchemon NFT Cards (Store: <a
+        href="https://www.randgallery.com/algo-collection/?address=ALCHY5SJXOXZXADZPD73KO6CYNZXDUWFYANTSXU6RIO3EZACIIXUCS3YDM"
+        target="_blank">NFTs)</a>
+      <br><br>Due to the fact that this is not a finished game, some operating systems attempt to block the download and
+      installation process, thinking that the game is a virus. You can feel free to run your own virus scan. We are in
+      the process of becoming a verified publisher.
+      <br><br>This game does not operate on mobile platforms, only laptops/desktops. Here are some tips for successfully
+      downloading and installing the video game:
+      <br><br>WINDOWS 10 USERS:
+      <br>1. If the file won't download, temporarily disable Windows Defender by following the instructions here: <a
+        href="https://support.microsoft.com/en-us/windows/turn-off-defender-antivirus-protection-in-windows-security-99e6004f-c54c-8509-773c-a4d776b77960"
+        target="_blank">Turn off Defender Antivirus Protection in Windows 10 Security</a>
+      <br>2. After you turn this off, the file should download with no issues. Once you open it, you may receive a
+      "Windows protected your PC" warning. If that happens, click "More info" and then select "Run."
+      <br>3. Turn Windows Defender back on.
+      <br><br>WINDOWS 11 USERS:
+      <br>1. If the file won't download, temporarily disable Windows Defender by following the instructions here: <a
+        href="https://allthings.how/how-to-enable-or-disable-microsoft-defender-in-windows-11/" target="_blank">How to
+        Enable or Disable Microsoft Defender in Windows 11</a>
+      <br>2. After you turn this off, the file should download with no issues. Once you open it, you may receive a
+      "Windows protected your PC" warning. If that happens, click "More info" and then select "Run."
+      <br>3. Turn Windows Defender back on.
+      <br><br>MAC USERS:
+      <br>After the file downloads, right click the download and select open. The first time you try to open it, you
+      just get a pop up warning that it can't be scanned for malicious files. However, if you click on it a second time,
+      the pop up displays an open button and you are able to click that and launch the game.
+      <br><br>DOWNLOAD LINK:
+      For Windows and Mac, you may also get notifications like "This download may not be safe," which will require you
+      to click "Keep" or "Download anyways."
+    </p> <br><br>
+    <a href="https://alch-executables.s3.amazonaws.com/AlchemonBeta2.0_Setup.exe" download>
+      <img src="../assets/pc.png" alt="Windows download" width="40%" height="auto"></a><br>
+    <a href="https://alch-executables.s3.amazonaws.com/AlchemonBeta2.zip" download>
+      <img src="../assets/mac.png" alt="Mac download" width="40%" height="auto">
+    </a>
+  </div>
 </template>
 
 <script>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -3,12 +3,50 @@
         <div class="blueBackground">
             <img id="moveAnimation_1" src="../assets/news.png" alt="News Character">
         </div>
+        <div>
+            <p class="orangeHeader spreadText">BETA 2.0 RELEASE - JULY 2022</p>
+            <p>
+                <u>WHATS NEW</u>
+            </p>
+            <p class="justifiedText">
+                - Leveling system: boost your Alchemon's power by leveling them up! Knock out enemies and win battles to
+                gain experience. Level caps set temporarily until V1 release.
+                <br>- Evolutions: Coming in 1.0 release.
+                <br>- Equipable NFTs: Purchase alchebility NFTs from our Website and equip them to your Alchemon!
+                Each
+                attack can be equipped to a single Alchemon of the same type (fire, water, psychic, etc).
+                <br>- Set 5 Alchemon: All set 5 cards are now implemented in game!
+                <br>- First community set: All Alchemon/Yieldy Flamingos have been added to the game! Once the set has
+                been
+                released to the public, they will become playable.
+                <br>- Improved rewards - Each win now rewards players with 6 ALCH. On top of this, each win will count
+                as a
+                single entry into the Hunting Grounds rewards lottery.
+                <br>- Forgot Password Functionality added.
+            </p>
+            <p>
+                <u>BUG FIXES</u>
+            </p>
+            <p class="justifiedText">
+                - Fixed issues related to various hover effects.
+                <br>- Balanced Alchemon animation's scale.
+                <br>- Fixed bug that prevented battle from ending when final Alchemon was affected with the Singed
+                debuff.
+                <br>- Multiple API optimizations made.
+                <br>- Various typos fixed.
+            </p>
+                <p>
+                <strong><br>If you have any feedback on Beta 2.0, please email feedback@alchemon.net</strong>
+            </p>
+            <img src="../assets/beta2.jpg" alt="Beta2.0">
+        </div>
         <div class="roadmap">
-            <p class="orangeHeader spreadText">JUNE 2022 UPDATE</p>
-            <p>Hello Alcheholics,
+            <p class="grayHeader spreadText">JUNE 2022 UPDATE</p>
+            <p class="whiteText">Hello Alcheholics,
                 <br><br>
             </p>
-            <p class="justifiedText">We have news & an updated roadmap for you. Due to the exploit we have had to
+            <p class="justifiedText whiteText">We have news & an updated roadmap for you. Due to the exploit we have had
+                to
                 increase security. Many of
                 these have already been developed in response, but the only real solution is to migrate the game logic
                 to a server, which we have been working on and making good progress. Until this is out in V1 we will
@@ -33,7 +71,7 @@
                 Clifford (CEO), Hanna (CTO) & Forest (Lead Dev) will be doing an AMA with Algo Gaming Guild on Weds June
                 15th at 3PM PST.
             </p>
-            <p><br><br><strong>2022 Roadmap (Updated)</strong>
+            <p class="whiteText"><br><br><strong>2022 Roadmap (Updated)</strong>
                 <br><br>
                 <u>Q2 (April-June)</u><br>
                 Set 5 Launch<br>
@@ -78,7 +116,7 @@
             </p><br><br>
         </div>
         <div class="oldRoadmap">
-            <p class="grayHeader spreadText">2022 ROADMAP</p>
+            <p class="yellowHeader spreadText">2022 ROADMAP</p>
             <p class="whiteText">Hello all my Alchemists!
                 <br><br>
                 Make sure you check out our new & improved <router-link to="/token">tokenomics</router-link> if you
@@ -140,9 +178,11 @@ img {
 .blueBackground {
     background-image: linear-gradient(to left, #007bff, #2A78F8, #4287F9, #89B4FB, #E7F0FE);
 }
-.oldRoadmap {
-    background-color: #ffa500;
+.roadmap {
+    background-color: #ffaf1c;
 }
+.oldRoadmap {
+    background-image: linear-gradient(to right, #007bff, #4287F9, #89B4FB);}
 p{
     text-align: center;
 }
