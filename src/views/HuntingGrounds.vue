952185df8d7f44cb0c12866b9c1e4c25ca253fcb
Hanna Wilson <57196027+hannalwilson@users.noreply.github.com>
2022-07-18 16:02:49 -0700

    hunting grounds updates

--- src/views/HuntingGrounds.vue
+++ src/views/HuntingGrounds.vue
@@ -1,24 +1,25 @@
 <template>
     <div class="huntinggrounds">
         <p class="whiteHeader spreadText">HUNTING GROUNDS</p>
-        <p>A seperate wallet to stake an extra Alchemon for a chance to earn prizes! Stake 1 Alchemon per wallet
-            here for 7 days and you earn entries for a raffle! You keep your Alchemon and can pull it out at any
-            time with a note on a 0.00 transaction, but only NFT Cards staked from Sunday until the drawing earn you
-            entries. The raffle will occur weekly and random winners will get the prizes determined by RNG! Winning
-            does not remove your entries, so one wallet can win multiple prizes each week. Good luck, you will need
-            it. Only time will tell what the lucky Alchemon will find in future weeks of the Hunting
-            Grounds.<br><br>&diams; Common = 1 Raffle Entry<br><br>&diams; Uncommon = 2 Raffle
-            Entries<br><br>&diams; Rare // Epic // Legendary = 3 Raffle Entries</p>
+        <p>The Hunting Grounds is evolving! Players no longer have to stake one of their Alchemon for a whole week to
+            earn entries in the Hunting Grounds raffle. Entries will now be earned through battling your Alchemon. Each battle won
+            equals one entry to the raffle and there is no limit to the number of entries any wallet can have. The raffle will occur
+             weekly and random winners will get the prizes determined by RNG! Winning does not remove your entries, so one wallet
+              can win multiple prizes each week. Good luck, you will need it.
+        </p>
         <img id="moveAnimation_2" src="https://alchemon-website-assets.s3.amazonaws.com/assets/hunting_grounds.png"
             alt="Bat" width="35%">
-        <p class="whiteHeader spreadText">WALLET ADDRESS FOR THE HUNTING GROUNDS: </p>
-        <p><b>HUNTMEC6NPJH7MLBUSMJQWZF5N5GPJBF6FJ5LYTRXR7NGLDBT5KB6453FA</b></p>
+        <p class="whiteHeader spreadText">HOW TO BATTLE </p>
+        <p>Players can build a team of three Alchemon and battle them in our desktop game, currently in Beta. Download the
+             <router-link to="/beta">game here</router-link> and then head over to our <router-link to="/howtobattle">Battle</router-link> page for instructions on
+              creating your account. Good luck!
+        </p>
         <img class="wallet_2" src="https://alchemon-website-assets.s3.amazonaws.com/assets/wallet_2.png" alt="Wallet"
             width="30%">
-        <p>Stake your Alchemon to be entered into an RNG raffle. Only one Alchemon staked per address. Any duplicate
-            entries from the same address will be sent back. Must stake starting on Sunday to earn the raffle
-            entries. There will be a weekly announcment of what prizes are in the raffle. Raffle will be
-            scheduled/paid out weekly on Sat/Sun.</p>
+        <p>If you still have an Alchemon staked in the Hunting Grounds, don't worry, those still count for raffle entries. We will be
+            sending those back to players over the next month. You can still send a 0.00 ALGO transaction with your Alchemon's name
+            in the notes to receive it back sooner, if you'd like.
+        </p>
         <img id="moveAnimation_3" src="https://alchemon-website-assets.s3.amazonaws.com/assets/hunting_grounds_1.png"
             alt="Demo" width="35%">
     </div>
