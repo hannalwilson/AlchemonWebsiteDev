778854cae8ea0bbadaca1245e92bff95c93806fd
Hanna Wilson <57196027+hannalwilson@users.noreply.github.com>
2022-11-07 15:53:18 -0600

    Revert "Merge branch 'main' into CreatingPacksSystem"
    This reverts commit 8306d020070ece720f1a7b6095203e20d5bcc674, reversing
    changes made to f742b668b9a7a8a41bc1fb9b02d93685a98a18e6.

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -1,97 +1,49 @@
+/* eslint-disable no-tabs */
 <template>
-  <li class="accordion__item">
-    <div class="accordion__trigger" :class="{ 'accordion__trigger_active': visible }" @click="open">
-
-      <slot name="accordion-trigger"></slot>
+    <div @click="toggleAnswer">
+        <p class="question">{{ question }}</p>
+        <div v-if="showAnswer" class="answer"><p v-html="answer"></p></div>
     </div>
-
-    <transition name="accordion" @enter="start" @after-enter="end" @before-leave="start" @after-leave="end">
-
-      <div class="accordion__content" v-show="visible">
-        <ul>
-          <slot name="accordion-content"></slot>
-        </ul>
-      </div>
-    </transition>
-  </li>
 </template>
 
 <script>
 export default {
-  props: {},
-  inject: ['Accordion'],
   data () {
     return {
-      index: null
-    }
-  },
-  computed: {
-    visible () {
-      return this.index === this.Accordion.active
+      showAnswer: false
     }
   },
   methods: {
-    open () {
-      if (this.visible) {
-        this.Accordion.active = null
-      } else {
-        this.Accordion.active = this.index
-      }
-    },
-    start (el) {
-      el.style.height = el.scrollHeight + 'px'
-    },
-    end (el) {
-      el.style.height = ''
+    toggleAnswer () {
+      this.showAnswer = !this.showAnswer
     }
   },
-  created () {
-    this.index = this.Accordion.count++
-  }
+  props: ['question', 'answer']
 }
 </script>
 
 <style lang="scss" scoped>
-.accordion__item {
-  cursor: pointer;
-  border-radius: 5px;
-  border: 1px orange solid;
-  border-bottom: none;
-  position: relative;
-  background: white;
-      color: rgb(0, 123, 255);
-        font-weight: 700;
-        text-transform: uppercase;
-        letter-spacing: normal;
-        padding-left: 1%;
-            width: 80vw;
-            margin: 0px auto;
+div {
+    background: white;
+    width: 80vw;
+    margin: 0 auto;
+    border-radius: 5px;
+    border: 1px orange solid;
+    border-bottom: none;
+    cursor: pointer;
+    overflow: hidden;
 }
-ul {
+.answer {
   background-color: orange;
-    background-image: linear-gradient(270deg, orange, rgb(255, 179, 39), rgb(255, 255, 255));
+    background-image: linear-gradient(to left, #ffa500, #ffb327,#ffffff);
     width: inherit;
     text-align: justify;
-    text-transform: none;
-    color: black;
-    font-weight: normal;
-    padding: 1%;
 }
-.accordion__trigger {
-  display: flex;
-  justify-content: space-between;
-}
-
-.accordion-enter-active,
-.accordion-leave-active {
-  will-change: height, opacity;
-  transition: height 0.3s ease, opacity 0.3s ease;
-  overflow: hidden;
-}
-
-.accordion-enter,
-.accordion-leave-to {
-  height: 0 !important;
-  opacity: 0;
+.question {
+  color: #007bff;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: normal;
+  padding-left: 1%;
 }
 </style>
