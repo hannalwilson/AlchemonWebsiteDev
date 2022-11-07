8c4215ac7010442a1d551ce3ef918c23f6e73faf
Hanna Wilson <57196027+hannalwilson@users.noreply.github.com>
2022-11-07 11:09:56 -0600

    replaced faq accordion
    found new code that closes one when another is opened

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -1,49 +1,99 @@
-/* eslint-disable no-tabs */
 <template>
-    <div @click="toggleAnswer">
-        <p class="question">{{ question }}</p>
-        <div v-if="showAnswer" class="answer"><p v-html="answer"></p></div>
+  <li class="accordion__item">
+    <div class="accordion__trigger" :class="{ 'accordion__trigger_active': visible }" @click="open">
+
+      <!-- This slot will handle the title/header of the accordion and is the part you click on -->
+      <slot name="accordion-trigger"></slot>
     </div>
+
+    <transition name="accordion" @enter="start" @after-enter="end" @before-leave="start" @after-leave="end">
+
+      <div class="accordion__content" v-show="visible">
+        <ul>
+          <!-- This slot will handle all the content that is passed to the accordion -->
+          <slot name="accordion-content"></slot>
+        </ul>
+      </div>
+    </transition>
+  </li>
 </template>
 
 <script>
 export default {
+  props: {},
+  inject: ['Accordion'],
   data () {
     return {
-      showAnswer: false
+      index: null
+    }
+  },
+  computed: {
+    visible () {
+      return this.index === this.Accordion.active
     }
   },
   methods: {
-    toggleAnswer () {
-      this.showAnswer = !this.showAnswer
+    open () {
+      if (this.visible) {
+        this.Accordion.active = null
+      } else {
+        this.Accordion.active = this.index
+      }
+    },
+    start (el) {
+      el.style.height = el.scrollHeight + 'px'
+    },
+    end (el) {
+      el.style.height = ''
     }
   },
-  props: ['question', 'answer']
+  created () {
+    this.index = this.Accordion.count++
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-div {
-    background: white;
-    width: 80vw;
-    margin: 0 auto;
-    border-radius: 5px;
-    border: 1px orange solid;
-    border-bottom: none;
-    cursor: pointer;
-    overflow: hidden;
+.accordion__item {
+  cursor: pointer;
+  border-radius: 5px;
+  border: 1px orange solid;
+  border-bottom: none;
+  position: relative;
+  background: white;
+      color: rgb(0, 123, 255);
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: normal;
+        padding-left: 1%;
+            width: 80vw;
+            margin: 0px auto;
 }
-.answer {
+ul {
   background-color: orange;
-    background-image: linear-gradient(to left, #ffa500, #ffb327,#ffffff);
+    background-image: linear-gradient(270deg, orange, rgb(255, 179, 39), rgb(255, 255, 255));
     width: inherit;
     text-align: justify;
+    text-transform: none;
+    color: black;
+    font-weight: normal;
+    padding: 1%;
 }
-.question {
-  color: #007bff;
-  font-weight: bold;
-  text-transform: uppercase;
-  letter-spacing: normal;
-  padding-left: 1%;
+.accordion__trigger {
+  display: flex;
+  justify-content: space-between;
+}
+
+.accordion-enter-active,
+.accordion-leave-active {
+  will-change: height, opacity;
+  transition: height 0.3s ease, opacity 0.3s ease;
+  overflow: hidden;
+}
+
+.accordion-enter,
+.accordion-leave-to {
+  height: 0 !important;
+  opacity: 0;
 }
 </style>
